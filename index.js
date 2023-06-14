97f429aea30215cc8daee14f590d97dc5fd33a02
Cole Rosner <colerosner@gmail.com>
2023-06-14 15:08:19 -0600

    Removed search bar, unused submitted fires layer

--- index.js
+++ index.js
@@ -2,16 +2,10 @@
 // Imports
 import WebMap from "@arcgis/core/WebMap.js";
 import MapView from '@arcgis/core/views/MapView.js';
-import FeatureLayer from '@arcgis/core/layers/FeatureLayer.js';
 import Home from "@arcgis/core/widgets/Home.js";
 import BasemapToggle from "@arcgis/core/widgets/BasemapToggle.js";
-import Search from "@arcgis/core/widgets/Search.js";
 
 // Script
-const commFires = new FeatureLayer({
-    url: "https://services1.arcgis.com/z5tlnpYHokW9isdE/arcgis/rest/services/community_fire_submissions/FeatureServer/0"
-});
-
 const webmap = new WebMap({
     portalItem: { // autocasts as new PortalItem()
       id: "72e8ab3919ad4f56ba0ab7abbe3aafae"
@@ -46,16 +40,6 @@ const homeWidget = new Home({
 view.ui.add(homeWidget, {
     position: "top-right",
     index: 1
-});
-
-// TODO: configure search for layers in map
-const searchWidget = new Search({
-    view: view
-});
-
-view.ui.add(searchWidget, {
-    position: "top-left",
-    index: 3
 });
 
 view.when(() => {
